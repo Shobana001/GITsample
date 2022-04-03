d1f05f89e1f2d40a8847094ad358f38c82e2530b
Shobana001 <sofi98543@gmail.com>
2022-04-03 22:52:37 +0530

    Adding Dell

---
+++ src/main/java/oro/gf/Dell.java
@@ -0,0 +1,13 @@
+package oro.gf;
+
+public class Dell {
+ private void number() {
+System.out.println(7);
+}
+ private void number5() {
+System.out.println("jbvj");
+}
+ public static void main(String[] args) {
+	
+}
+}
